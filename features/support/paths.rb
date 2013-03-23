59889262aeffd4e1f466ea7beeafd5f5b5a228b5
zmejia <zaidamejia@gmial.com>
2013-03-23 13:53:48 -0700

    Hw3 part1

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,8 +13,11 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the (RottenPotatoes )?home\s?page$/ then '/movies'
-    when /^the movies page$/ then '/movies'
+    when /^the home\s?page$/
+      '/'
+
+    when /^the RottenPotatoes home page/
+      '/movies'
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
